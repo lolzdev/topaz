84ee267b5da8be94e7e5b17298ce1f101c79cc67
Lorenzo Torres <lorenzo@sagittarius-a.org>
2025-11-02 20:58:04 +0100

    added fatal() in log.h

--- core/log.h
+++ core/log.h
@@ -3,6 +3,7 @@
 #define LOG_H
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #define log_error_code(msg) do { printf("\x1b[31m" "[ERROR]" "\x1b[0m" "(" __FILE__ ":" __LINE__ "): %s", (msg)); } while (0)
 #define log_warning_code(msg) do { printf("\x1b[33m" "[WARN]" "\x1b[0m" "(" __FILE__ ":" __LINE__ "): %s", (msg)); } while (0)
@@ -11,5 +12,7 @@
 #define log_error(msg) do { printf("\x1b[31m" "[ERROR]" "\x1b[0m: %s", (msg)); } while (0)
 #define log_warning(msg) do { printf("\x1b[33m" "[WARN]" "\x1b[0m: %s", (msg)); } while (0)
 #define log_info(msg) do { printf("\x1b[32m" "[INFO]" "\x1b[0m: %s", (msg)); } while (0)
+
+#define fatal(msg) do { log_error(msg); exit(1); } while (0)
 
 #endif
